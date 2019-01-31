0fe33795f170f190c225c369cd33b086632e53b6
venkatasusmitha <45706251+venkatasusmitha@users.noreply.github.com>
2019-01-31 11:30:50 +0530

    Update 22.py

--- 22.py
+++ 22.py
@@ -1,13 +1,13 @@
-num = int(input())
+num2 = int(input())
  
-temp = num
+temp = num2
 rev = 0
  
 while temp != 0:
     rev = (rev * 10) + (temp % 10)
     temp = temp // 10
  
-if num == rev:
+if num2 == rev:
     print("yes")
 else:
     print("no")
