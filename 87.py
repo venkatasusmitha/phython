013d6062c3324820351ae483d6c1cfc543f4815b
venkatasusmitha <45706251+venkatasusmitha@users.noreply.github.com>
2019-05-13 14:31:19 +0530

    Update 87.py

--- 87.py
+++ 87.py
@@ -1,10 +1,10 @@
 q=raw_input()
 c=len(q)
 fct=0
-for i in range(0,c):
-    j=i+1
+for i1 in range(0,c):
+    j=i1+1
     while j<c:
-        if ord(q[i])==ord(q[j]):
+        if ord(q[i1])==ord(q[j]):
             fct=1
         j=j+1
 if fct!=1:
