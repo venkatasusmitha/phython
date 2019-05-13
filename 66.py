fc690a8a083f2178e42afa87ad082c54ed5bf9f4
venkatasusmitha <45706251+venkatasusmitha@users.noreply.github.com>
2019-05-13 13:29:14 +0530

    Update 66.py

--- 66.py
+++ 66.py
@@ -1,6 +1,6 @@
-num = int(raw_input())
-for i in range(2, int(num/2)):
-    if num % i  == 0:
+num3= int(raw_input())
+for i in range(2, int(num3/2)):
+    if num3 % i  == 0:
         print("no")
         break
 else:
