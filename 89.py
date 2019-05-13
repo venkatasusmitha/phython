6261ee1b5599a67211e247e9f7574d22a504e93e
venkatasusmitha <45706251+venkatasusmitha@users.noreply.github.com>
2019-05-13 14:36:19 +0530

    Update 89.py

--- 89.py
+++ 89.py
@@ -1,9 +1,9 @@
 def sort(values):
-	for  i in range(len(values)):		
-		for r in range(i, len(values)):			
-			if (values[i] > values[r]):
-				temp = values[i]
-				values[i] = values[r]
+	for  i1 in range(len(values)):		
+		for r in range(i1, len(values)):			
+			if (values[i1] > values[r]):
+				temp = values[i1]
+				values[i1] = values[r]
 				values[r] = temp
 x = raw_input().rstrip()
 xList = list(x)
