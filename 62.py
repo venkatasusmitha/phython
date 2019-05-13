b6e8f955242298f428010d7bd977c5794dd37d2c
venkatasusmitha <45706251+venkatasusmitha@users.noreply.github.com>
2019-05-13 13:18:02 +0530

    Update 62.py

--- 62.py
+++ 62.py
@@ -1,10 +1,10 @@
-q=raw_input()
+x=raw_input()
 """if all(c in '01' for c in q):
     print "Yes"
 else:
     print "No"
     """
-if not(q.translate(None,'01')):
+if not(x.translate(None,'01')):
     print "Yes"
 else:
     print "No"
